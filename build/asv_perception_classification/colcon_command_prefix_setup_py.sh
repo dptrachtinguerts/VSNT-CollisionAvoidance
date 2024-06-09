e82fbb6303e14425a240fac6a9c5ecb731fad3b4
dptrachtinguerts <diegoptrach@gmail.com>
2024-06-09 18:10:44 -0300

    Remove tracking for build, install and log directories

--- build/asv_perception_classification/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/vicente/VSNT-CollisionAvoidance/install/asv_perception_common/share/asv_perception_common/package.sh"
-. "/home/vicente/VSNT-CollisionAvoidance/install/asv_perception_interfaces/share/asv_perception_interfaces/package.sh"
